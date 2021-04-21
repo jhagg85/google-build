17c23d2ba0abc43fb6c2a42783908d33f3c2765d
jhagg85 <jhaggy85@gmail.com>
2021-04-21 17:58:01 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="flex flex-col">
+        Let's Build Google with Next.js and Tailwind
+      </h1>
+    </div>
+  );
+}
